69572c2036187bd8082fca47050460c39b7baa37
tttdashen <2429043178@qq.com>
2025-05-07 10:11:28 +0800

    Day2：路径参数与查询参数

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#day1
 from fastapi import FastAPI #调用函数，fastapi是文件名（不含.py），FastAPI是函数名
 # 创建 FastAPI 应用实例
 app = FastAPI()
@@ -20,3 +21,18 @@ def get_user(name:str):
 访问 /user/jack，返回：
 {"username": "jack"}
 '''
+#day2
+#步骤一：定义路径参数接口
+@app.get("/users/{user_id}")#/users/{user_id}：路径中带变量
+def read_user(user_id:int):#user_id: int：自动转换类型，访问 /users/123 会传入 123#
+    return{"user_id":user_id}#FastAPI 会自动帮你处理参数类型，错误类型（如 /users/abc）会返回 422 错误
+
+#步骤二：定义带查询参数的接口
+@app.get("/items")
+def read_items(skip:int=0,limit:int=10):#可以设置默认值，不传参数也不会报错
+    #访问 /items/?skip=5&limit=20.skip 和 limit 是查询参数（URL 中 ? 后面的部分）
+    return{"skip":skip,"limit":limit}
+
+@app.get("/books/{book_id}")
+def get_book(book_id:int,detail:bool=False):#要求输入的book_id为整数型，输入的datail为布尔型
+    return{"book_id":book_id,"detail":detail}
